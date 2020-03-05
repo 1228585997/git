6e3dfde9af877b36f4037907bff22478f92c3a04
Your Name <you@example.com>
2020-03-05 09:11:18 +0800

    提交

--- app.js
+++ app.js
@@ -3,5 +3,4 @@ const app = express()
 
 app.use(express.static('./dist'))
 
-// app.listen(8888, () => {console.log('sever running on http://localhost:8888')})
-app.listen(8888, () => {console.log('启动端口 http://localhost:8888')})
+app.listen(8888, () => {console.log('sever running at http://localhost:8888')})
